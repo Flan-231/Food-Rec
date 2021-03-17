d089c771c64652978a3e8178e7cdbabd8223f280
asal101 <77691090+asal101@users.noreply.github.com>
2021-03-16 20:20:26 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,18 +8,14 @@ def render_main():
 
 @app.route("/response")
 def render_response():
-    food = request.args['pizza'] 
-    food = request.args['burgers'] 
+    food = request.args['food'] 
     #The request object stores information about the request sent to the server.
     #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
     #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
-    if food == 'pizza':
+    if food == 'pink':
         reply = "Some good pizza places are Rustys, Dominos, Little Caesars."
-        
-    elif food == 'burgers':
-        reply = "Some good burger places are In-n-Out, Habit, and Island Burger."
     else:
-        reply = "Please try another type of food."
+        reply = "Please try another food type."
     return render_template('response.html', response = reply)
     
 if __name__=="__main__":
