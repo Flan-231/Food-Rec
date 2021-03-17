2e12eb49ce070702a410b0732a246f7ecdbc35b9
asal101 <77691090+asal101@users.noreply.github.com>
2021-03-16 21:24:01 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -14,8 +14,10 @@ def render_response():
     #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
     if food == 'pizza':
         reply = "Some good pizza places are Rustys, Dominos, Little Caesars."
-    elif food == 'burger':
+    if food == 'burgers':
         reply = "Some good burger places are In-n-Out, Habit, and Island Burger."
+    if food == 'ice cream':
+        reply = "Some good ice cream places are Mission Street Ice Cream, Mcconnells, and Cold Stone."
     else:
         reply = "Please try another food type."
     return render_template('response.html', response = reply)
